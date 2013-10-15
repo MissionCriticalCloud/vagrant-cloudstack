5f51918db0e426545c9796166932fdab215f520d
Roeland Kuipers <roeland@boul.nl>
2013-10-16 00:41:53 +0200

    fix rsync on windows
    https://github.com/mitchellh/vagrant-aws/pull/77/files

--- lib/vagrant-cloudstack/action/sync_folders.rb
+++ lib/vagrant-cloudstack/action/sync_folders.rb
@@ -4,17 +4,19 @@ require "vagrant/util/subprocess"
 
 require "vagrant/util/scoped_hash_override"
 
+require "vagrant/util/which"
+
 module VagrantPlugins
-  module Cloudstack
+  module CloudStack
     module Action
       # This middleware uses `rsync` to sync the folders over to the
-      # cloudstack instance.
+      # AWS instance.
       class SyncFolders
         include Vagrant::Util::ScopedHashOverride
 
         def initialize(app, env)
           @app    = app
-          @logger = Log4r::Logger.new("vagrant_cloudstack::action::sync_folders")
+          @logger = Log4r::Logger.new("vagrant_aws::action::sync_folders")
         end
 
         def call(env)
@@ -23,10 +25,15 @@ module VagrantPlugins
           ssh_info = env[:machine].ssh_info
 
           env[:machine].config.vm.synced_folders.each do |id, data|
-            data = scoped_hash_override(data, :cloudstack)
+            data = scoped_hash_override(data, :aws)
 
             # Ignore disabled shared folders
             next if data[:disabled]
+
+            unless Vagrant::Util::Which.which('rsync')
+              env[:ui].warn(I18n.t('vagrant_aws.rsync_not_found_warning'))
+              break
+            end
 
             hostpath  = File.expand_path(data[:hostpath], env[:root_path])
             guestpath = data[:guestpath]
@@ -35,7 +42,12 @@ module VagrantPlugins
             # avoid creating an additional directory with rsync
             hostpath = "#{hostpath}/" if hostpath !~ /\/$/
 
-            env[:ui].info(I18n.t("vagrant_cloudstack.rsync_folder",
+            # on windows rsync.exe requires cygdrive-style paths
+            if Vagrant::Util::Platform.windows?
+              hostpath = hostpath.gsub(/^(\w):/) { "/cygdrive/#{$1}" }
+            end
+
+            env[:ui].info(I18n.t("vagrant_aws.rsync_folder",
                                 :hostpath => hostpath,
                                 :guestpath => guestpath))
 
@@ -51,6 +63,12 @@ module VagrantPlugins
               "-e", "ssh -p #{ssh_info[:port]} -o StrictHostKeyChecking=no -i '#{ssh_info[:private_key_path]}'",
               hostpath,
               "#{ssh_info[:username]}@#{ssh_info[:host]}:#{guestpath}"]
+
+            # we need to fix permissions when using rsync.exe on windows, see
+            # http://stackoverflow.com/questions/5798807/rsync-permission-denied-created-directories-have-no-permissions
+            if Vagrant::Util::Platform.windows?
+              command.insert(1, "--chmod", "ugo=rwX")
+            end
 
             r = Vagrant::Util::Subprocess.execute(*command)
             if r.exit_code != 0
