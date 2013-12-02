5aa208d6bb40d1076c036387f51588b2a94b1687
Olle Lundberg <geek@nerd.sh>
2013-12-02 14:11:07 +0100

    Enable parallelization.

--- lib/vagrant-cloudstack/plugin.rb
+++ lib/vagrant-cloudstack/plugin.rb
@@ -24,8 +24,7 @@ module VagrantPlugins
         Config
       end
 
-      provider(:cloudstack) do
-        # Setup logging and i18n
+      provider(:cloudstack, parallel: true) do        # Setup logging and i18n
         setup_logging
         setup_i18n
 
