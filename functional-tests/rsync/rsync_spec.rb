748ed1526e470760fea108d84434c65c92c6e165
Bob <bvandenheuvel@schubergphilis.com>
2016-03-07 15:23:21 +0100

    RSync test checks `/vagrant`

--- functional-tests/rsync/rsync_spec.rb
+++ functional-tests/rsync/rsync_spec.rb
@@ -2,7 +2,7 @@ describe 'VM RSync' do
   it 'does rsync to the VM' do
     expect(`vagrant up`).to include('Machine is booted and ready for use!')
     expect($?.exitstatus).to eq(0)
-    expect(`vagrant ssh -c "ls /etc; echo;"`).to include('Vagrantfile.advanced_networking')
+    expect(`vagrant ssh -c "ls /vagrant; echo;"`).to include('Vagrantfile.advanced_networking')
     expect(`vagrant destroy --force`).to include('Terminating the instance...')
     expect($?.exitstatus).to eq(0)
   end
