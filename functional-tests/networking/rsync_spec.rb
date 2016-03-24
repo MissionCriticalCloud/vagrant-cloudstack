5893e87976f60748f4d3d93ec2258b6ae1d069fa
Bob <bvandenheuvel@schubergphilis.com>
2016-03-24 10:29:23 +0100

    Allow trusted_networks as both string or array

--- functional-tests/networking/rsync_spec.rb
+++ functional-tests/networking/rsync_spec.rb
@@ -1,6 +1,9 @@
 describe 'Networking features' do
   it 'creates firewall and portwarding rules' do
-    expect(`vagrant up`).to include('Machine is booted and ready for use!')
+    expect(`vagrant up`).to include(
+      'box1: Machine is booted and ready for use!',
+      'box2: Machine is booted and ready for use!'
+    )
     expect($?.exitstatus).to eq(0)
 
     expect(`vagrant destroy --force`).to include('Terminating the instance...')
