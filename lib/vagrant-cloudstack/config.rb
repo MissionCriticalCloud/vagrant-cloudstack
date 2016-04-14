9d224abe1aa133f7ae3ea7e8b04add896e8048b6
sue445 <sue445@sue445.net>
2016-04-14 16:27:17 +0900

    Add ssh_network_name to config

--- lib/vagrant-cloudstack/config.rb
+++ lib/vagrant-cloudstack/config.rb
@@ -227,6 +227,11 @@ module VagrantPlugins
       # @return [String]
       attr_accessor :ssh_network_id
 
+      # The network_name to be used when loging in to the vm via ssh
+      #
+      # @return [String]
+      attr_accessor :ssh_network_name
+
       # The username to be used when loging in to the vm
       #
       # @return [String]
@@ -485,6 +490,9 @@ module VagrantPlugins
 
         # ssh network_id is nil by default
         @ssh_network_id         = nil if @ssh_network_id == UNSET_VALUE
+
+        # ssh network_name is nil by default
+        @ssh_network_name       = nil if @ssh_network_name == UNSET_VALUE
 
         # vm user is nil by default
         @vm_user               = nil if @vm_user == UNSET_VALUE
