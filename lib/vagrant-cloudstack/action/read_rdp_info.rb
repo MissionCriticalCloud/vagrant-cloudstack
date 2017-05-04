8343f0280fdf00d3e2e889296abae0fbe961d034
Bob van den Heuvel <bvandenheuvel@schubergphilis.com>
2017-05-04 13:49:47 +0200

    Consolidate code to super class for read_(rdp|ssh|winrm)_info

--- lib/vagrant-cloudstack/action/read_rdp_info.rb
+++ lib/vagrant-cloudstack/action/read_rdp_info.rb
@@ -1,3 +1,4 @@
+require_relative 'read_transport_info.rb'
 require 'log4r'
 
 module VagrantPlugins
@@ -5,10 +6,12 @@ module VagrantPlugins
     module Action
       # This action reads the WinRM info for the machine and puts it into the
       # `:machine_winrm_info` key in the environment.
-      class ReadRdpInfo
+      class ReadRdpInfo < ReadTransportInfo
         def initialize(app, env)
           @app    = app
           @logger = Log4r::Logger.new("vagrant_cloudstack::action::read_rdp_info")
+
+          @public_port_fieldname = 'pf_public_rdp_port'
         end
 
         def call(env)
@@ -33,42 +36,14 @@ module VagrantPlugins
           domain        = machine.provider_config.domain_id
           domain_config = machine.provider_config.get_domain_config(domain)
 
-          pf_ip_address_id   = domain_config.pf_ip_address_id
-          pf_ip_address      = domain_config.pf_ip_address
-          pf_public_rdp_port = domain_config.pf_public_rdp_port
+          pf_ip_address, pf_public_port = retrieve_public_ip_port(cloudstack, domain_config, machine)
 
-          if pf_public_rdp_port.nil?
-            pf_public_rdp_port_file = machine.data_dir.join('pf_public_rdp_port')
-            if pf_public_rdp_port_file.file?
-              File.read(pf_public_rdp_port_file).each_line do |line|
-                pf_public_rdp_port = line.strip
-              end
-              domain_config.pf_public_rdp_port = pf_public_rdp_port
-            end
-          end
-
-          if not pf_ip_address and pf_ip_address_id and pf_public_rdp_port
-            begin
-              response = cloudstack.list_public_ip_addresses({:id => pf_ip_address_id})
-            rescue Fog::Compute::Cloudstack::Error => e
-              raise Errors::FogError, :message => e.message
-            end
-
-            if response["listpublicipaddressesresponse"]["count"] == 0
-              @logger.info("IP address #{pf_ip_address_id} not exists.")
-              env[:ui].info(I18n.t("IP address #{pf_ip_address_id} not exists."))
-              pf_ip_address = nil
-            else
-              pf_ip_address = response["listpublicipaddressesresponse"]["publicipaddress"][0]["ipaddress"]
-            end
-          end
-
-          rdp_info = {
+          transport_info = {
                        :host => pf_ip_address || server.nics[0]['ipaddress'],
-                       :port => pf_public_rdp_port
+                       :port => pf_public_port
                      }
 
-          rdp_info
+          transport_info
         end
       end
     end
