b84dd0ff61bcc411c72dabfa588493564cb016f0
Bob van den Heuvel <bvandenheuvel@schubergphilis.com>
2017-05-17 17:32:20 +0200

    Add unit test for retrieve_public_ip_port_spec

--- spec/vagrant-cloudstack/action/retrieve_public_ip_port_spec.rb
+++ spec/vagrant-cloudstack/action/retrieve_public_ip_port_spec.rb
@@ -1,94 +1,93 @@
 require 'spec_helper'
-require 'vagrant-cloudstack/action/read_ssh_info'
+require 'vagrant-cloudstack/action/read_transport_info'
 require 'vagrant-cloudstack/config'
+require 'fog'
 
-describe VagrantPlugins::Cloudstack::Action::ReadSSHInfo do
-  let(:action) {VagrantPlugins::Cloudstack::Action::ReadSSHInfo.new(nil, nil)}
+describe VagrantPlugins::Cloudstack::Action::ReadTransportInfo do
+  let(:action) {VagrantPlugins::Cloudstack::Action::ReadTransportInfo.new }
 
-  describe '#fetch_nic_ip_address' do
-    subject {action.fetch_nic_ip_address(nics, domain_config)}
+  describe '#retrieve_public_ip_port' do
+    subject { action.retrieve_public_ip_port(cloudstack_compute, domain_config, machine) }
 
-    let(:nics) do
-      [
-          {'networkid' => 'networkid1', 'networkname' => 'networkname1', 'ipaddress' => '127.0.0.1'},
-          {'networkid' => 'networkid2', 'networkname' => 'networkname2', 'ipaddress' => '127.0.0.2'},
-          {'networkid' => 'networkid3', 'networkname' => 'networkname3', 'ipaddress' => '127.0.0.3'},
-      ]
-    end
+    let(:cloudstack_compute) { double('Fog::Compute::Cloudstack') }
+    let(:machine) { double('Vagrant::Machine')}
 
-    let(:ssh_network_id) {Vagrant::Plugin::V2::Config::UNSET_VALUE}
-    let(:ssh_network_name) {Vagrant::Plugin::V2::Config::UNSET_VALUE}
+    let(:data_dir) { double('Pathname') }
+    let(:pf_public_port_file) { double('Pathname') }
+
+    let(:pf_ip_address) { 'ip_address_in_config' }
+    let(:pf_ip_address_from_server) { 'ip_address_from_server' }
+    let(:pf_ip_address_id) { 'ID of ip_address_in_config' }
+    let(:pf_public_port) { 'public_port_in_config' }
+    let(:pf_public_port_from_file) { 'public_port_from_file' }
 
     let(:domain_config) do
       config = VagrantPlugins::Cloudstack::Config.new
-      config.domain_config :cloudstack do |cloudstack|
-        cloudstack.ssh_network_id = ssh_network_id
-        cloudstack.ssh_network_name = ssh_network_name
+      config.domain_config :cloudstack do |cfg|
+        cfg.pf_ip_address = pf_ip_address
+        cfg.pf_public_port = pf_public_port
+        cfg.pf_ip_address_id = pf_ip_address_id
       end
       config.finalize!
       config.get_domain_config(:cloudstack)
     end
 
-    context 'without neither ssh_network_id and ssh_network_name' do
-      it {should eq '127.0.0.1'
-      }
-    end
-
-    context 'with ssh_network_id' do
-      context 'when exists in nics' do
-        let(:ssh_network_id) {'networkid2'
-        }
-
-        it {should eq '127.0.0.2'
-        }
-      end
-
-      context 'when not exists in nics' do
-        let(:ssh_network_id) {'unknown'
-        }
-
-        it {should eq '127.0.0.1'
-        }
+    context 'without both ip address and port in config' do
+      it 'retrieves those configured values' do
+        should eq [pf_ip_address, pf_public_port]
       end
     end
 
-    context 'with ssh_network_id' do
-      context 'when exists in nics' do
-        let(:ssh_network_name) {'networkname3'
-        }
+    context 'port not configured' do
+      let(:pf_public_port) { nil }
 
-        it {should eq '127.0.0.3'
-        }
-      end
+      it 'retrieves the active port stored on filesystem' do
+        expect(machine).to receive(:data_dir).and_return(data_dir)
+        expect(data_dir).to receive(:join).and_return(pf_public_port_file)
+        expect(pf_public_port_file).to receive(:file?).and_return(true)
+        expect(File).to receive(:read).and_return(pf_public_port_from_file)
 
-      context 'when not exists in nics' do
-        let(:ssh_network_name) {'unknown'
-        }
-
-        it {should eq '127.0.0.1'
-        }
+        expect(subject).to eq [pf_ip_address, pf_public_port_from_file]
       end
     end
 
-    context 'with both ssh_network_id and ssh_network_name' do
-      context 'when exists in nics' do
-        let(:ssh_network_id) {'networkid2'
-        }
-        let(:ssh_network_name) {'networkname3'
-        }
+    context 'only ID of ip address specified (and public port)' do
+      let(:pf_ip_address) { nil }
 
-        it {should eq '127.0.0.2'
+      it 'resolves, and returns, the ip address from the ID' do
+        response = {
+          'listpublicipaddressesresponse' => {
+            'count' =>1,
+            'publicipaddress' =>[{
+              'id' => pf_ip_address_id,
+              'ipaddress' => pf_ip_address_from_server,
+              'allocated' => '2016-05-06T13:58:04+0200',
+              'zoneid' => 'UUID',
+              'zonename' => 'Name',
+              'issourcenat' =>false,
+              'account' => 'Name',
+              'domainid' => 'UUID',
+              'domain' => 'Name',
+              'forvirtualnetwork' =>true,
+              'isstaticnat' =>false,
+              'issystem' =>false,
+              'associatednetworkid' => 'UUID',
+              'associatednetworkname' => 'Name',
+              'networkid' => 'UUID',
+              'aclid' => 'UUID',
+              'state' => 'Allocated',
+              'physicalnetworkid' => 'UUID',
+              'vpcid' => 'UUID',
+              'tags' =>[],
+              'isportable' =>false
+            }]
+          }
         }
-      end
+        expect(cloudstack_compute).to receive(:list_public_ip_addresses)
+          .with(:id => pf_ip_address_id)
+          .and_return(response)
 
-      context 'when not exists in nics' do
-        let(:ssh_network_id) {'unknown'
-        }
-        let(:ssh_network_name) {'unknown'
-        }
-
-        it {should eq '127.0.0.1'
-        }
+        expect(subject).to eq [pf_ip_address_from_server, pf_public_port]
       end
     end
   end
